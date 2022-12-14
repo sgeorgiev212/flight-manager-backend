48652fac4f6d6f80a4bf85d34038ffbbe9b9fc58
Svetoslav Georgiev <sv.georgiev23@gmail.com>
2022-12-14 12:53:38 +0200

    fixed exception handler

--- src/main/java/com/example/demo/exception/ExceptionHandlerConfig.java
+++ src/main/java/com/example/demo/exception/ExceptionHandlerConfig.java
@@ -3,11 +3,12 @@ package com.example.demo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class ExceptionHandlerConfig {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = {IllegalArgumentException.class})
+    @ExceptionHandler(value = {IllegalArgumentException.class})
     public ResponseEntity<Object> handleIllegalAgrumentException(IllegalArgumentException exception) {
          return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
